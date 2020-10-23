ab8da9227bd7eefb2b1282e235fed17cb682088e
alechudson <42881696+alechudson@users.noreply.github.com>
2020-10-22 22:30:31 -0500

    first commit

---
+++ powerlogger.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <SD.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+const int chipSelect = 3;
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     LED_BUILTIN // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  //boot message
+  display.clearDisplay();
+  delay(100);
+  display.setCursor(0,0);
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.println("Hello Word");
+  display.display();
+  delay(1000);
+  display.println("This is a test");
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+}
+
+void loop() {
+}
