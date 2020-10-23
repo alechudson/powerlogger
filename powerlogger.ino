8020123a40f658d23586a9f3dba86090f59f2538
alechudson <42881696+alechudson@users.noreply.github.com>
2020-10-22 23:11:30 -0500

    Added basic RTC functionality

--- powerlogger.ino
+++ powerlogger.ino
@@ -3,8 +3,14 @@
 #include <SD.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "RTClib.h"
 
 const int chipSelect = 3;
+
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -16,14 +22,21 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 void setup() {
   Serial.begin(9600);
 
-  /////Setup for the display
+/////Setup for the RTC
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+
+/////Setup for the display
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
 
-  /////Setup for the SD card
+/////Setup for the SD card
     Serial.print("Initializing SD card...");
  
   // see if the card is present and can be initialized:
@@ -53,7 +66,9 @@ void setup() {
 
 void loop() {
 
-   // open the file. note that only one file can be open at a time,
+
+/////Writing the data to SD card
+  // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
 
@@ -68,6 +83,29 @@ void loop() {
   else {
     Serial.println("error opening datalog.txt");
   }
+
+
+/////RTC Time and temp
+    DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print("Temperature: ");
+    Serial.print(rtc.getTemperature());
+    Serial.println(" C");
 
   delay(5000);
   
